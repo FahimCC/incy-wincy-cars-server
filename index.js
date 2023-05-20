5839bdde4971b11ef997023ee953e4e238c46898
unknown <ffnasif@gmail.com>
2023-05-20 22:11:12 +0600

    server is deployed in vercel & created a route that will send trending toy data to client

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -8,8 +9,46 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('Incy Wincy Cars server is running.');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ws55k5x.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+
+		const trendingToy = client.db('incyWincyCars').collection('trendingToy');
+
+		//for trending toy data
+		app.get('/:sub_category', async (req, res) => {
+			const subCategory = req.params.sub_category;
+			const query = { sub_category: subCategory };
+			const result = await trendingToy.find(query).toArray();
+			res.send(result);
+		});
+
+		// Send a ping to confirm a successful connection
+		await client.db('admin').command({ ping: 1 });
+		console.log(
+			'Pinged your deployment. You successfully connected to MongoDB!'
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		// await client.close();
+	}
+}
+run().catch(console.dir);
+
+app.get('/', async (req, res) => {
+	res.send('Incy Wincy Cars is running...');
 });
 
 app.listen(port, () => {
