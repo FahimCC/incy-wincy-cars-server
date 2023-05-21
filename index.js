b5a3aee906ce73e5ca61ba9f950a25a6a647ad3f
unknown <ffnasif@gmail.com>
2023-05-21 08:39:25 +0600

    created add_toy route for receiving data from client

--- index.js
+++ index.js
@@ -23,15 +23,36 @@ const client = new MongoClient(uri, {
 async function run() {
 	try {
 		// Connect the client to the server	(optional starting in v4.7)
-		await client.connect();
+		client.connect();
 
-		const trendingToy = client.db('incyWincyCars').collection('trendingToy');
+		const toyCollection = client
+			.db('incyWincyCars')
+			.collection('toyCollection');
 
 		//for trending toy data
 		app.get('/:sub_category', async (req, res) => {
 			const subCategory = req.params.sub_category;
-			const query = { sub_category: subCategory };
-			const result = await trendingToy.find(query).toArray();
+			const query = { subCategory: subCategory };
+			const result = await toyCollection.find(query).project({photoURL:1,toyName:1,price:1,ratings:1}).limit(2).toArray();
+			res.send(result);
+		});
+
+		app.post('/add_toy', async (req, res) => {
+			// const toyData = req.body;
+
+			const doc = {
+				photoURL: req.body.photoURL,
+				toyName: req.body.toyName,
+				sellerName: req.body.sellerName,
+				sellerEmail: req.body.sellerEmail,
+				subCategory: req.body.subCategory,
+				price: req.body.price,
+				ratings: req.body.ratings,
+				availableQuantity: req.body.availableQuantity,
+				detailsDescription: req.body.detailsDescription,
+			};
+
+			const result = await toyCollection.insertOne(doc);
 			res.send(result);
 		});
 
